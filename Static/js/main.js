9a1f40b7783828723b00b10914d87a94db9a6117
Leandro Farias <leoffarias@hotmail.com>
2015-05-17 19:54:11 -0300

    paginas internas

---
+++ Static/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+$( ".btn-show" ).click(function() {
+  $(this).next().slideToggle( "slow" );
+});
+
+});
--- Website/views.py
+++ Website/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def atualizarperfil(request):
+    return render(request, 'atualizarperfil.html')
