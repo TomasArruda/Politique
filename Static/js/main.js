83e5d99b66cd293cfa50d45e33503991bc8679fe
brunoassisp <bap2@cin.ufpe.br>
2015-05-21 22:16:23 -0300

    Alteracoes em padroes e templates

--- Static/js/main.js
+++ Static/js/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
 
-$( ".btn-show" ).click(function() {
-  $(this).next().slideToggle( "slow" );
-});
+	$( ".btn-show" ).click(function() {
 
+	  	$(this).next().slideToggle( "slow" );
+
+	  	if($(this).hasClass("consulta")){
+
+			if($(this).hasClass("last")){
+		  		$(this).removeClass("last");
+			} else {
+			  	$(this).addClass("last");
+			}	
+		} 
+	});
 });
