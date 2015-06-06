d561c21256009f827e5b46a87a6d5df4d42cde09
brunoassisp <bap2@cin.ufpe.br>
2015-06-06 17:29:15 -0300

    Telas de Cadastro prontas para uso

--- Website/views/__init__.py
+++ Website/views/__init__.py
@@ -1,6 +1,7 @@
 from membro import cadastrarMembroView
 from setor import SetorView
 from evento import EventoView
+from evento import CadastrarEventoView
 from telefone import TelefoneView
 from financiamento import FinanciamentoView
 from iniciativa import IniciativaView
@@ -8,6 +9,7 @@ from iniciativa import CadastrarIniciativaView
 from iniciativa import EditarIniciativaView
 from tipoParceria import TipoParceriaView
 from empresaParceira import EmpresaParceiraView
+from empresaParceira import CadastrarEmpresaView
 from contatoIniciativa import ContatoIniciativaView
 from contatoCapacitacao import ContatoCapacitacaoView
 from capacitacaoExterna import CapacitacaoExternaView
--- Website/views/evento.py
+++ Website/views/evento.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 from Website.models import Evento
-
 
 def EventoView(request):
     query = Evento.objects.order_by('nome')
     #context = RequestContext(request, {'latest_question_list': latest_question_list,})
     #render(request, '/membros.html', context)
+
+def CadastrarEventoView(request):
+	return render(request, 'cadastrarEvento.html')
