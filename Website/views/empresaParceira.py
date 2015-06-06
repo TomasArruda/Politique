8bc063aee20e4edf77d46294b3e11f06a90e9e9f
brunoassisp <bap2@cin.ufpe.br>
2015-06-06 10:51:59 -0300

    Branch defeituosa

--- Website/views/empresaParceira.py
+++ Website/views/empresaParceira.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 from Website.models import EmpresaParceira
 
 
@@ -7,3 +7,6 @@ def EmpresaParceiraView(request):
     query = EmpresaParceira.objects.order_by('nome')
     #context = RequestContext(request, {'latest_question_list': latest_question_list,})
     #render(request, '/membros.html', context)
+
+def CadastrarEmpresaView(request):
+	return render(request, 'cadastrarEmpresa.html')
