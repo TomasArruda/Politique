94c15bccdc342ac1c2090cb710ac85dee910839e
lucasvtenorio <Lucasvtenorio@gmail.com>
2015-06-06 14:54:09 -0300

    Mais commits de tommer 2

--- Website/forms/membro.py
+++ Website/forms/membro.py
@@ -2,9 +2,8 @@ from django.contrib.auth.models import User
 from django import forms
 from Website.models import Membro
 
-class MembroForm(forms.Form):
+class MembroForm(forms.ModelForm):
 	nome = forms.CharField()
 	email = forms.CharField()
-	password = forms.CharField()
-	telefone = forms.CharField()
+password = forms.CharField()
 	setor = forms.CharField()
