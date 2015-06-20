10c037f882d2bb2b657f1fd14eec7e1c21d6dc08
tomasarruda <tomas.arruda@gmail.com>
2015-06-20 10:18:10 -0300

    correcoes login logout

--- Website/views/autenticacao.py
+++ Website/views/autenticacao.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 
 def LogoutView(request):
     logout(request)
@@ -12,10 +13,8 @@ def LoginView(request):
     if user is not None:
         if user.is_active:
             login(request, user)
-            return redirect('/Website/Perfil')
+            return HttpResponseRedirect('/Website/Perfil')
         else:
-            x = 0
-            # Return a 'disabled account' error message
+            return HttpResponseRedirect('/Website')
     else:
-        x = 0
-        # Return an 'invalid login' error message.
+        return HttpResponseRedirect('/Website')
